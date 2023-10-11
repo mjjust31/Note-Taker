6760a25be5638c1f637dea056c20d4d3eedc7572
justmann <mj.justmann@gmail.com>
2023-10-11 17:55:41 -0500

    setting-up-variables-for paths

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,27 @@
 //set up express
 
-const express = require('express')
-const app = express()
-
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.get('/goodbye', (req,res)=>{
-//   res.send("goodbye")
-// })
-
-
-
+const express = require("express");
+const app = express();
+const htmlRoute = require('./routes/route-html'); 
+const apiRoute = require('./routes/route-api');
 const PORT = process.env.port || 3001;
+// app.use("/", (req, res) => {
+//   res.send(`<h1> ${Date()} </h1> `)
+// })
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(htmlRoute);
+app.use(apiRoute);
+
+
+// '/api/notes'
+// `/api/notes/${id}`,
+// /notes'
+
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Example app listening on port ${PORT}`);
+});
+
