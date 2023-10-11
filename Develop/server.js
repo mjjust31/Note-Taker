d1659e716c0e0b148141cb171b5f89b279eda3ff
justmann <mj.justmann@gmail.com>
2023-10-11 17:13:30 -0500

    setting-up-static-folder

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,20 @@
-const express = require('express');
+//set up express
+
+const express = require('express')
+const app = express()
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.get('/goodbye', (req,res)=>{
+//   res.send("goodbye")
+// })
+
+
+
 const PORT = process.env.port || 3001;
-const app = express();
-
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
-
-//set up a static folder.
-app.use(express.static('public'));
-
-
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
