81f1218033d4e42e355d1cc520d4233e00db3e31
justmann <mj.justmann@gmail.com>
2023-10-18 16:52:51 -0500

    README-created

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Note Taker Starter Code
 
+## Description
 
+### User Story
 
-
-## User Story 
 AS A small business owner
 I WANT to be able to write and save notes
 SO THAT I can organize my thoughts and keep track of tasks I need to complete
+### Acceptance Criteria
 
-## Acceptance Criteria
 GIVEN a note-taking application
 WHEN I open the Note Taker
 THEN I am presented with a landing page with a link to a notes page
@@ -22,3 +22,27 @@ WHEN I click on an existing note in the list in the left-hand column
 THEN that note appears in the right-hand column and a "New Note" button appears in the navigation
 WHEN I click on the "New Note" button in the navigation at the top of the page
 THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column and the button disappears
+
+## Installation
+
+Installed the following packages to assist the building of the note taker-app:
+a. uuid
+b.nomdemon
+c.express
+
+To start the application, run "npm start".
+
+## Contributing
+
+To assist me with understanding the path files, I watched the Express part of Node.js, Express, MongoDB & More: The Complete Bootcamp 2023 from Udemy to gain a deeper understand of how to approach file paths. This helped me develope a sense of the file strucutre along with the notes from the NU Bootcamp.
+
+I also feel like there is an easier way to delete the note. After much trial and error, it will work, but you have to press delete, and then refresh the page. I tried to do a forced window load after the delete path, but it didn't work. Although it will delete the task after pressing delete and then refreshing, I couldn't get it to do it after clicking the trash icon.
+
+
+## License
+
+MIT License
+
+## Questions
+
+For any questions, please contact me at mj.justmann@gmail.com
--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.get("/notes", function (req, res) {
   });
 });
 
-app.get("/api/notes", (req, res) => {
+const getNotes = (req, res) => {
   return res.json(noteData);
-});
+};
 
-app.post("/api/notes", (req, res) => {
+const postNotes = (req, res) => {
   console.info(`${req.method} received`);
 
   const { title, text } = req.body;
@@ -55,11 +55,11 @@ app.post("/api/notes", (req, res) => {
       res.status(201).json(response);
     });
   }
-});
+};
 
 //get a single note based on generated id
 // console.log(noteData[0].note_id)
-app.get(`/api/notes/:id`, (req, res) => {
+const getNoteId = (req, res) => {
   const noteId = req.params.id;
   if (noteId) {
     for (let i = 0; i < noteData.length; i++) {
@@ -72,24 +72,50 @@ app.get(`/api/notes/:id`, (req, res) => {
   } else {
     return res.status(400).send("Note ID not provided");
   }
-});
+};
 
-// DELETE /api/notes/:id
+//DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete.
+// In order to delete a note, you'll need to read all notes. X
+// from the db.json file, remove the note with the given id property,
+//and then rewrite the notes to the db.json file.
 
-app.delete("/api/notes/:id", (req, res) => {
+const deleteNote = (req, res) => {
   const noteId = req.params.id;
   if (noteId) {
     for (let i = 0; i < noteData.length; i++) {
       const currentNote = noteData[i];
       if (currentNote.id === noteId) {
-        return res.status(204).json({ title: null, text: null, id: null });
+        const updatedNotes = noteData.filter((note) => note.id !== noteId);
+        const stringUpdatedNotes = JSON.stringify(updatedNotes);
+        fs.writeFile("./db/db.json", stringUpdatedNotes, (err) => {
+          if (err) {
+            res.status(500).send("Error writing file.");
+            return;
+          }
+          res.status(200).send("Note deleted successfully.");
+          window.reload();
+        });
       }
     }
-    return res.status(404).send("Note not found");
-  } else {
-    return res.status(400).send("Note ID not provided");
   }
-});
+};
+
+// noteData.push(newNote);
+// const stringNoteData = JSON.stringify(noteData);
+
+// const response = {
+//   status: "success",
+//   body: newNote,
+// };
+
+// fs.writeFile("./db/db.json", stringNoteData, (err) => {
+//   res.status(201).json(response);
+// });
+
+app.route("/api/notes").get(getNotes).post(postNotes);
+app.route(`/api/notes/:id`).get(getNoteId).delete(deleteNote);
+
+// DELETE /api/notes/:id
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
